36b709d048ee47744d9d54855c69256ec03e943c
lovric_g <goran.lovric@gnudo.com>
2014-02-28 17:07:52 +0100

    added automatic uncertainty calculations for energy and source sizes

---
+++ examples/uncertainty_calc.m
@@ -0,0 +1,41 @@
+% This is a wrapper function for calculating the energy and source size
+% uncertainties with the Newton method. The fitting function has to be run
+% first and the results have to be saved into <results.mat>.
+%--------------------------------------------------------------------------
+% Date: 2014-02-28
+% Author: Goran Lovric
+% License: GPL 3 (see LICENSE file in root folder)
+%--------------------------------------------------------------------------
+close all;clc;clear;
+addpath('../classes');
+addpath('../classes/xray-interaction-constants');
+addpath('../scripts');
+
+load('results.mat');
+
+del_z   = 0.002;                % [m] propagation distance uncertainty
+
+% starting values for Newton searches
+E_start = ene.val.*1.05;        % [keV] starting value of E
+sigma_H_start = src_H.val*1.5;  % [m] starting value of simga_H
+sigma_V_start = src_V.val*1.5;  % [m] starting value of simga_V
+
+%--------------------------------------------------------------------------
+% 1.) Calculate physical uncertainties
+%--------------------------------------------------------------------------
+[del_E del_sigma]= errorcalc(a,del_z,ene.val,E_start, ...
+                            [src_H.val src_V.val], ...
+                            [sigma_H_start sigma_V_start], ...
+                            [duty_H.val duty_V.val],[ang_H.val ang_V.val]);
+
+%--------------------------------------------------------------------------
+% 2.) Add uncertainties from fitting procedure
+%--------------------------------------------------------------------------                        
+del_E_eff = del_E + ene.del;
+del_sigma_eff(1) = del_sigma(1) + src_H.del;
+del_sigma_eff(2) = del_sigma(2) + src_V.del;
+
+%--------------------------------------------------------------------------
+% 2.) Save all into results.mat
+%--------------------------------------------------------------------------
+save('results.mat','del_E_eff','del_sigma_eff','-append')
---
+++ scripts/errorcalc.m
@@ -0,0 +1,73 @@
+% Function for calculating the energy and source size uncertainties
+% according to Eqs. (19) and (20) from the paper by Lovric et al.,
+% Opt. Express 22, 2745 (2014). For finding the solution, we make use of
+% the Newton method (http://en.wikipedia.org/wiki/Newton_method).
+%--------------------------------------------------------------------------
+% Date: 2014-02-28
+% Author: Goran Lovric
+% License: GPL 3 (see LICENSE file in root folder)
+%--------------------------------------------------------------------------
+function [del_E,del_sigma] = errorcalc(a,del_z,E,E_start,sigma, ...
+                                                      sigma_start,dc,alpha)
+
+% (1) First we calculate the left side of Eq. (19)
+[F_simH F_simV] = a.calcFcoeff2D(E,[sigma(1) sigma(2)],...
+                            [dc(1) dc(2)],[alpha(1) alpha(2)]);
+
+a.z = a.z + 0.002;
+[F_simH2 F_simV2] = a.calcFcoeff2D(E,[sigma(1) sigma(2)],...
+                            [dc(1) dc(2)],[alpha(1) alpha(2)]);
+                        
+p_H = a.weightedLSE(F_simH,F_simH2);
+p_V = a.weightedLSE(F_simV,F_simV2);
+
+a.z = a.z - 0.002;
+
+eps   = 0.01;
+f_H = 1;
+f_v = f_H;
+ff  = f_H;
+
+% (2) Now we search for del_sigma of the right side from Eq. (19)
+sigma_H_start = sigma_start(1);
+sigma_V_start = sigma_start(2);
+
+while f_H > eps | f_H > eps
+d_sigma_H = (sigma_H_start - sigma(1)) .* [1 1.05];
+d_sigma_V = (sigma_V_start - sigma(2)) .* [1 1.05];
+
+for ii=1:2
+    [F_simH2 F_simV2] = a.calcFcoeff2D(E,[sigma(1)+d_sigma_H(ii) ...
+                                       sigma(2)+d_sigma_V(ii)], ...
+                            [dc(1) dc(2)],[alpha(1) alpha(2)]);
+	del_p_H(ii) = a.weightedLSE(F_simH2,F_simH) - p_H;
+    del_p_V(ii) = a.weightedLSE(F_simV2,F_simV) - p_V;
+end
+f_H = del_p_H(1);
+f_V = del_p_V(1);
+
+f_H_prime = (del_p_H(2)-del_p_H(1))/(d_sigma_H(2)-d_sigma_H(1));
+f_V_prime = (del_p_V(2)-del_p_V(1))/(d_sigma_V(2)-d_sigma_V(1));
+sigma_H_start = sigma_H_start - del_p_H(1) ./ f_H_prime;
+sigma_V_start = sigma_V_start - del_p_V(1) ./ f_V_prime;
+end
+
+del_sigma = [d_sigma_H(1) d_sigma_V(1)];
+
+% (3) We repeat the same for Eq. (20)
+while ff > eps
+d_E = (E_start - E) .* [1 1.05];
+
+for ii=1:2
+    [F_simH2 F_simV2] = a.calcFcoeff2D(E+d_E(ii),[sigma(1) sigma(2)], ...
+                            [dc(1) dc(2)],[alpha(1) alpha(2)]);
+	del_p(ii) = a.weightedLSE(F_simH2,F_simH) - max([p_H p_V]);
+end
+ff = del_p(1);
+
+ff_prime = (del_p(2)-del_p(1))/(d_E(2)-d_E(1));
+E_start = E_start - del_p(1) ./ ff_prime;
+end
+
+del_E = d_E(1)*3;  % according to Eq. (26)
+end
